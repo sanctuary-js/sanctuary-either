6edacd88693eddbdde72227591e071041be26d0f
David Chambers <dc@davidchambers.me>
2019-03-05 19:20:25 +0100

    unify environment detection

--- index.js
+++ index.js
@@ -20,17 +20,24 @@
 
   'use strict';
 
+  var util = {inspect: {}};
+
   /* istanbul ignore else */
   if (typeof module === 'object' && typeof module.exports === 'object') {
-    module.exports = f (require ('sanctuary-show'),
+    module.exports = f (require ('util'),
+                        require ('sanctuary-show'),
                         require ('sanctuary-type-classes'));
   } else if (typeof define === 'function' && define.amd != null) {
-    define (['sanctuary-show', 'sanctuary-type-classes'], f);
+    define (['sanctuary-show', 'sanctuary-type-classes'], function(show, Z) {
+      return f (util, show, Z);
+    });
   } else {
-    self.sanctuaryEither = f (self.sanctuaryShow, self.sanctuaryTypeClasses);
+    self.sanctuaryEither = f (util,
+                              self.sanctuaryShow,
+                              self.sanctuaryTypeClasses);
   }
 
-} (function(show, Z) {
+} (function(util, show, Z) {
 
   'use strict';
 
@@ -88,16 +95,15 @@
     /* eslint-enable key-spacing */
   };
 
-  var util =
-    typeof module === 'object' && typeof module.exports === 'object' ?
-    require ('util') :
-    /* istanbul ignore next */ {};
-  var inspect =
-    util.inspect != null && typeof util.inspect.custom === 'symbol' ?
-    /* istanbul ignore next */ util.inspect.custom :
-    /* istanbul ignore next */ 'inspect';
-  Left$prototype[inspect] = Left$prototype$show;
-  Right$prototype[inspect] = Right$prototype$show;
+  var custom = util.inspect.custom;
+  /* istanbul ignore else */
+  if (typeof custom === 'symbol') {
+    Left$prototype[custom] = Left$prototype$show;
+    Right$prototype[custom] = Right$prototype$show;
+  } else {
+    Left$prototype.inspect = Left$prototype$show;
+    Right$prototype.inspect = Right$prototype$show;
+  }
 
   //. `Either a b` satisfies the following [Fantasy Land][] specifications:
   //.
