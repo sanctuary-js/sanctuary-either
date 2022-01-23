534be1074b9a1d24c54e8a8c060c5127dabdb7cf
David Chambers <dc@davidchambers.me>
2022-01-23 19:21:45 +0100

    test: use ES modules

--- test/index.js
+++ test/index.js
@@ -1,16 +1,14 @@
-'use strict';
+import assert       from 'assert';
 
-const assert        = require ('assert');
+import laws         from 'fantasy-laws';
+import jsc          from 'jsverify';
+import Identity     from 'sanctuary-identity';
+import show         from 'sanctuary-show';
+import Z            from 'sanctuary-type-classes';
+import type         from 'sanctuary-type-identifiers';
+import Useless      from 'sanctuary-useless';
 
-const laws          = require ('fantasy-laws');
-const jsc           = require ('jsverify');
-const Identity      = require ('sanctuary-identity');
-const show          = require ('sanctuary-show');
-const Z             = require ('sanctuary-type-classes');
-const type          = require ('sanctuary-type-identifiers');
-const Useless       = require ('sanctuary-useless');
-
-const Either        = require ('..');
+import Either       from '../index.js';
 
 
 const {Left, Right} = Either;
